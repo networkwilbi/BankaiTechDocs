e3f9acee70bd4dfe8ba3ebbfce83162e4da9a151
TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>
2024-12-08 20:18:27 -0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -145,7 +145,7 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'sidebar',
           position: 'left',
           label: 'Tutorials',
         },
