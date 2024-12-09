c036c7443ee2b81b6ead81c02cb96b7ccdd0a2fa
TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>
2024-12-09 01:53:39 -0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           showLastUpdateTime: true,
           routeBasePath: '/', // Serve the docs at the site's root
           sidebarCollapsible: true,
-          sidebarPath: './sidebars/docs.ts',
+          sidebarPath: './sidebars/customSidebar.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
