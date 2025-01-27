c89479b4d81c7e5922a3c6b7b04cbff71d890df5
TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>
2025-01-27 11:20:23 -0500

    Update docusaurus.config.ts
    add additional languages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -235,6 +235,7 @@ const config: Config = {
     prism: {
       darkTheme: prismThemes.dracula,
       theme: prismThemes.github,
+      additionalLanguages: ['dax', 'csharp', 'powerquery', 'powershell', 'yaml', 'nginx'],
     },
   } satisfies Preset.ThemeConfig,
 };
